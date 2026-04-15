70b2cd7204ce911eb230e0206d08776339c24c14
jayraj-mak <jayraj.makwana@techahir.com>
2026-04-15 09:11:50 +0000

    changes

--- 01-Basics/Test1.js
+++ 01-Basics/Test1.js
@@ -1,1 +1,8 @@
 console.log("Jayraj");
+console,log("Makwana");
+console,log("harshil");
+console,log("umesh");
+console,log("Prince");
+console,log("Krishan");
+console,log("Devarsh");
+console,log("Arun");
